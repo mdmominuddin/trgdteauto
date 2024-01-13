93cc5004a98f71cb29162e15ea90f9a91b736462
unknown <momin.milon@outlook.com>
2024-01-13 22:51:42 +0600

    added collecstatic

--- afdtrgauto/settings.py
+++ afdtrgauto/settings.py
@@ -24,9 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)!o61bz&n-mrsnx2^r@yqkcd_qud+db-bke-yw=eunktoxig0('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
+DEBUG = False
 ALLOWED_HOSTS = ["officeatuo.onrender.com"]
+
+# DEBUG = True
+# ALLOWED_HOSTS = []
 
 
 # Application definition
