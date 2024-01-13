d44d4e60fa491766657c41eaa703cc76c4531daf
unknown <momin.milon@outlook.com>
2024-01-13 22:02:09 +0600

    added gunicorn

--- afdtrgauto/settings.py
+++ afdtrgauto/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)!o61bz&n-mrsnx2^r@yqkcd_qud+db-bke-yw=eunktoxig0('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["officeatuo.onrender.com"]
 
 
 # Application definition
